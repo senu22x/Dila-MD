1a51a45fe4eabf7a2820406bc658c9bc971d4af5
senu22x <senudisanjana58@gmail.com>
2024-10-17 09:50:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "QWUE1YhY#kvSs6ijE3tRuW_5JOmfjYB-tGoG98DjYClGOceISnbs",
+SESSION_ID: process.env.SESSION_ID || "eTx3TQKB#bIVzMzT9iqxS47-UKyqC2TvLniXmbG89fz1SHsfRXSo",
 MONGODB: process.env.MONGODB || "mongodb://mongo:ZPsEFsCqNDkRKgOLzBjUCOkBccGsjKLE@junction.proxy.rlwy.net:11132",
 };
